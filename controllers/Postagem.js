6bbd767aa3092f65fc7e77f65603f48488666e12
patriciasnt <54919582+patriciasnt@users.noreply.github.com>
2019-11-12 12:31:26 -0300

    Add files via upload

--- controllers/Postagem.js
+++ controllers/Postagem.js
@@ -1,1 +1,22 @@
+const PostagemModel = require("../models/Postagem")
 
+
+module.exports = {
+  salvarNoticias: async function (req, res) {
+    const novaNoticia = {
+      titulo: req.fields.titulo,
+      imagem: req.files.imagem.path.split("\\")[2]
+    }
+    const noticia = await PostagemModel.persistir(novaNoticia)
+
+    res.redirect("/")
+  },
+  listarNoticias: async function (req, res) {
+    const noticias = await PostagemModel.carregarTodas()
+    res.render('index', noticias)
+  },
+  buscarNoticias: async function (titulo) {
+    const noticias = await PostagemModel.buscar(titulo)
+    return noticias
+  }
+}
